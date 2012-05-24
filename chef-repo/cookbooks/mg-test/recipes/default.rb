9dada184795c611c5063d9aa7cecdaea088ac85a
Manas Gupta <manas@easilydo.com>
2012-05-24 09:06:25 -0400

    Committing to bump version

--- chef-repo/cookbooks/mg-test/recipes/default.rb
+++ chef-repo/cookbooks/mg-test/recipes/default.rb
@@ -9,7 +9,29 @@ git repo do
     action :sync
     depth 5
     notifies :create, "ruby_block[a_block]"
+    notifies :execute, "execute[clean]"
+    notifies :execute, "execute[build]"
 end
+
+#build the dirty way!
+app_name = "Simple"
+deploy_dir = "/var/#{app_name}"
+build_dir = "#{deploy_dir}/build"
+
+directory deploy_dir
+
+execute "build" do
+    command "mkdir #{build_dir}; \
+            javac #{repo}/code/*.java -d #{build_dir}"
+    creates build_dir
+    action :nothing
+end
+
+execute "clean" do
+    command "rm -rf #{build_dir}"
+    action :nothing
+end
+
 
 ruby_block "a_block" do
     block do
