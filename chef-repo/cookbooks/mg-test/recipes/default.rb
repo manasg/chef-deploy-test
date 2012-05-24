fa1f373be0392c44e7c3a01b82abd1f5bf55515f
Manas Gupta <manas@easilydo.com>
2012-05-24 08:43:09 -0400

    Testing notifies

--- chef-repo/cookbooks/mg-test/recipes/default.rb
+++ chef-repo/cookbooks/mg-test/recipes/default.rb
@@ -8,4 +8,12 @@ git repo do
     repository "https://github.com/manasg/chef-deploy-test"
     action :sync
     depth 5
+    notifies :create, "ruby_block[a_block]"
+end
+
+ruby_block "a_block" do
+    block do
+        log "I was created"
+    end
+    action :nothing
 end
