7a4e3b1076c63e955f5ec6c0fde9e76481e8b6dd
Manas Gupta <manas@easilydo.com>
2012-05-24 09:11:48 -0400

    Changing to bump version

--- code/Simple.java
+++ code/Simple.java
@@ -1,5 +1,6 @@
 class Simple {
     public static void main(String[] args) {
         System.out.println("Dont you love java!? Cmon?!!");
+        System.out.println("I prefer Ruby for what I do");
     }
 }
